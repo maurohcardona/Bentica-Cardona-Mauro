4a7181b7df28048bdd152b712ba879d6e7f25396
maurohcardona <maurohcardona@gmail.com>
2022-11-06 13:55:41 -0300

    haciendo el carrito

--- src/Componentes/Cart.jsx
+++ src/Componentes/Cart.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { useContext } from "react";
 import { CartContext } from '../Context/CartContext'
-
 
 function Cart() {
 
-    
-    
+    const carrito = useContext(CartContext)
+    console.log(carrito)
     return(
         <div>    
-            <h1>carrito</h1>
+            <h1>Carrito</h1>
+            <div>
+                {carrito.cart.map((prod) => (
+                    <div>
+                        <p>{prod.nombre}</p>
+                        <p>{prod.cantidad}</p>
+                    </div>
+                ))
+                }
+            </div>
             
         </div>
     );
--- src/Componentes/CartWidget.jsx
+++ src/Componentes/CartWidget.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import {RiShoppingCartLine} from 'react-icons/ri'
 import '../Estilos/Header.css'
-
+import { useContext } from "react";
+import { CartContext } from '../Context/CartContext'
 
 
 function CartWidget () {
+    
+    const carrito = useContext(CartContext)
+    
     return (
         <div className="carrito" > 
             <RiShoppingCartLine color="black"  size= {35}/>
+            <span>{carrito.cart.length}</span>
         </div>
     )
 }
